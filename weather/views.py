ceac79ca1eb72d728ee5408a36e063c7b0bad65a
Snehashis Dasgupta <snehashis.jisu.cse@gmail.com>
2023-01-07 19:12:26 +0500

    Weather detector using django.

---
+++ weather/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+import json, urllib.request
+
+# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        city = request.POST['city']
+        res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=f0d5b3864676bc1af2864bf6846283bc').read()
+        json_data = json.loads(res)
+        data = {
+            "country_code": str(json_data['sys']['country']),
+            "coordinate": str(json_data['coord']['lon']) + ' ' + str(json_data['coord']['lat']),
+            "temp": str(json_data['main']['temp']-273.15) + ' °C',
+            "pressure": str(json_data['main']['pressure']),
+            "humidity": str(json_data['main']['humidity']),
+        }
+
+    else:
+        city = ''
+        data = {}
+    return render(request, 'index.html',  {'city': city, 'data': data})
